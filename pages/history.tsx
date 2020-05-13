a5793afcd7207c0c90e6416f46e36365e1baa791
Sandra Filipsson <sandra.filipsson@connectitude.se>
2020-05-13 16:06:03 +0200

    start of navigation

---
+++ pages/history.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTranslation, UseTranslationResponse, I18nextProvider } from 'react-i18next';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function HistoryComponent() {
+
+    const { t }: UseTranslationResponse = useTranslation();
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.separate}>{t('introduction')}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    separate: {
+        marginTop: 50,
+    },
+});
